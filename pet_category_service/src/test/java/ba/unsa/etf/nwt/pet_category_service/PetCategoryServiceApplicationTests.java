d18416dd2594ca59c3114d43c72fedbe39e6d7fd
SamraMujcinovic <smujcinovi1@etf.unsa.ba>
2021-03-14 20:51:30 +0100

    pet_category_service

---
+++ pet_category_service/src/test/java/ba/unsa/etf/nwt/pet_category_service/PetCategoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ba.unsa.etf.nwt.pet_category_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetCategoryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
