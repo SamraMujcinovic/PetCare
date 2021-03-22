ef5a1c0fa432e2e1d5c0c5968979ba1ab12cbd63
Amila Lakovic <alakovic1@etf.unsa.ba>
2021-03-22 02:21:35 +0100

    implemented tests for validations

---
+++ user_service/src/test/java/ba/unsa/etf/nwt/user_service/AnswerTests.java
@@ -0,0 +1,12 @@
+package ba.unsa.etf.nwt.user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+public class AnswerTests {
+
+    @Test
+    void test1() {
+    }
+}
