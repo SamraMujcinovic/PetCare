03306de85f7a95fa62c595540464a92355df774f
SamraMujcinovic <smujcinovi1@etf.unsa.ba>
2021-03-27 16:12:21 +0100

    dodan swagger

---
+++ pet_category_service/src/main/java/ba/unsa/etf/nwt/pet_category_service/swager/Swager2Config.java
@@ -0,0 +1,37 @@
+package ba.unsa.etf.nwt.pet_category_service.swager;
+
+import com.google.common.collect.Lists;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.*;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import java.util.List;
+
+@Configuration
+@EnableSwagger2
+public class Swager2Config {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2).select()
+                .apis(RequestHandlerSelectors
+                        .basePackage("ba.unsa.etf.nwt.pet_category_service.controllers"))
+                .paths(PathSelectors.regex("/.*"))
+                .build().apiInfo(apiEndPointsInfo());
+    }
+    private ApiInfo apiEndPointsInfo() {
+        return new ApiInfoBuilder().title("Pet Category Service Documentation")
+                .description("Pet Care pet_category service")
+                .license("Apache 2.0")
+                .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
+                .version("1.0.0")
+                .build();
+    }
+
+}
