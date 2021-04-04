35f92f1ffc97385d0379cb93933a04290b66f195
Amila Lakovic <alakovic1@etf.unsa.ba>
2021-04-04 03:27:41 +0200

    encryption works for server, but not for clients

--- user_service/src/main/java/ba/unsa/etf/nwt/user_service/controller/ConfigController.java
+++ user_service/src/main/java/ba/unsa/etf/nwt/user_service/controller/ConfigController.java
@@ -1,15 +1,17 @@
 package ba.unsa.etf.nwt.user_service.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RefreshScope
+@ConfigurationProperties("variable")
 public class ConfigController {
     //Value and GetMapping for config server
-    @Value("${my.variable: default value}")
+    //@Value("${variable}")
     private String variable;
 
     @GetMapping("/configserver/test")
