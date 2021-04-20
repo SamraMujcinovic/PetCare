dd599838dad905a1684b46b1c02e50446a1231b8
Amila Lakovic <alakovic1@etf.unsa.ba>
2021-04-20 04:36:06 +0200

    grpc connected to adopt_service

---
+++ adopt_service/src/main/java/ba/unsa/etf/nwt/adopt_service/interceptor/GRPCInterceptor.java
@@ -0,0 +1,45 @@
+package ba.unsa.etf.nwt.adopt_service.interceptor;
+
+import ba.unsa.etf.nwt.adopt_service.service.GRPCService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+public class GRPCInterceptor implements HandlerInterceptor {
+    @Autowired
+    private GRPCService grpcService;
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception exception) throws Exception {
+
+        String responseType = "";
+
+        if(response.getStatus() == 200){
+            responseType = "OK";
+        }
+        else if(response.getStatus() == 404){
+            responseType = "ERROR - ResourceNotFound";
+        }
+        else {
+            responseType = "ERROR - WrongInput/Validation";
+        }
+
+        try {
+            //ne racunaj swagger requeste
+            if (!request.getRequestURI().substring(0, 8).equals("/swagger") &&
+                    !request.getRequestURI().substring(0, 8).equals("/webjars")) {
+
+                grpcService.save(request.getMethod(), request.getRequestURI(), responseType);
+            }
+        }
+        catch (Exception e){
+            //za slucajeve kada je duzina requesta manja od 8
+            grpcService.save(request.getMethod(), request.getRequestURI(), responseType);
+        }
+
+    }
+}
